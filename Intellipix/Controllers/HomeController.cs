55da2f714624236fbe6441989277669d04e9f29e
Ronald Driscoll <rdriscoll6220@outlook.com>
2020-09-02 23:39:17 -0700

    Add project files.

---
+++ Intellipix/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using ImageResizer;
+using Intellipix.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Intellipix.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            // Pass a list of blob URIs in ViewBag
+            CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("photos");
+            List<BlobInfo> blobs = new List<BlobInfo>();
+
+            foreach (IListBlobItem item in container.ListBlobs())
+            {
+                var blob = item as CloudBlockBlob;
+
+                if (blob != null)
+                {
+                    blobs.Add(new BlobInfo()
+                    {
+                        ImageUri = blob.Uri.ToString(),
+                        ThumbnailUri = blob.Uri.ToString().Replace("/photos/", "/thumbnails/")
+                    });
+                }
+            }
+
+            ViewBag.Blobs = blobs.ToArray();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Make sure the user selected an image file
+                if (!file.ContentType.StartsWith("image"))
+                {
+                    TempData["Message"] = "Only image files may be uploaded";
+                }
+                else
+                {
+                    try
+                    {
+                        // Save the original image in the "photos" container
+                        CloudStorageAccount account = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+                        CloudBlobClient client = account.CreateCloudBlobClient();
+                        CloudBlobContainer container = client.GetContainerReference("photos");
+                        CloudBlockBlob photo = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                        await photo.UploadFromStreamAsync(file.InputStream);
+
+                        // Generate a thumbnail and save it in the "thumbnails" container
+                        using (var outputStream = new MemoryStream())
+                        {
+                            file.InputStream.Seek(0L, SeekOrigin.Begin);
+                            var settings = new ResizeSettings { MaxWidth = 192 };
+                            ImageBuilder.Current.Build(file.InputStream, outputStream, settings);
+                            outputStream.Seek(0L, SeekOrigin.Begin);
+                            container = client.GetContainerReference("thumbnails");
+                            CloudBlockBlob thumbnail = container.GetBlockBlobReference(Path.GetFileName(file.FileName));
+                            await thumbnail.UploadFromStreamAsync(outputStream);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // In case something goes wrong
+                        TempData["Message"] = ex.Message;
+                    }
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+    
+}
